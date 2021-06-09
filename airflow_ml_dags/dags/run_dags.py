a13b30ee6a264965a56c77de5986dd40541a5b78
JessikaSmith <mariyaxod@yandex.ru>
2021-06-09 18:33:30 +0300

    initial structure

---
+++ airflow_ml_dags/dags/run_dags.py
@@ -0,0 +1,14 @@
+import os
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    "owner": "airflow",
+    "email": ["airflow@example.com"],
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
