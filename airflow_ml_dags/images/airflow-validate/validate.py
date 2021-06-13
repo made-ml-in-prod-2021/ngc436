e7fa35d2de7479dbbc16cd5f3d95d7175db9a79f
ngc436 <mariyaxod@yandex.ru>
2021-06-14 00:27:09 +0300

    Add validation

--- airflow_ml_dags/images/airflow-validate/validate.py
+++ airflow_ml_dags/images/airflow-validate/validate.py
@@ -1,19 +1,30 @@
 import os
 import click
+import pandas as pd
 from joblib import load
 import logging
 import yaml
 
 DEFAUL_LOGGING_CONFIG_FILEPATH = "logging.conf.yaml"
 
-# TODO: set logger
+
 def setup_logging():
     """Reading logger config from yaml"""
     with open(DEFAUL_LOGGING_CONFIG_FILEPATH) as config_fin:
         logging.config.dictConfig(yaml.safe_load(config_fin))
 
+
 @click.command("validate")
 @click.option("--input-dir")
-@click.option("--output-dir")
-def validate(input_dir: str, output_model_dir: str):
-    pass
+@click.option("--model-dir")
+def validate(input_dir: str, model_dir: str):
+    setup_logging()
+    logger_metrics = logging.getLogger("validation_metrics")
+
+    val_X = pd.read_csv(os.path.join(input_dir, "val_X.csv"))
+    val_y = pd.read_csv(os.path.join(input_dir, "val_y.csv"))
+    model = load("model.joblib")
+    model_ds = model_dir.split('/')[-1]
+    score = model.score(val_X, val_y)
+
+    logger_metrics.info(f'got metric {score} for model {model_ds}')
