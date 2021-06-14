b02c61524cf6919f27eb1602dc8d278577286ed9
ngc436 <mariyaxod@yandex.ru>
2021-06-14 18:13:03 +0300

    Fix validation

--- airflow_ml_dags/images/airflow-validate/validate.py
+++ airflow_ml_dags/images/airflow-validate/validate.py
@@ -8,9 +8,14 @@ import yaml
 import mlflow
 from mlflow.tracking import MlflowClient
 
+from airflow.models import Variable
+
+EXP_NAME = Variable.get("EXP_NAME")
+MODEL_NAME = Variable.get("MODEL_NAME")
 DEFAUL_LOGGING_CONFIG_FILEPATH = "logging.conf.yaml"
 
 mlflow.set_tracking_uri("http://localhost:5000")
+
 
 def setup_logging():
     """Reading logger config from yaml"""
@@ -27,12 +32,30 @@ def validate(input_dir: str, model_dir: str):
 
     val_X = pd.read_csv(os.path.join(input_dir, "val_X.csv"))
     val_y = pd.read_csv(os.path.join(input_dir, "val_y.csv"))
-    model = load("model.joblib")
+    # model = load("model.joblib")
 
     client = MlflowClient()
     exp = client.get_experiment_by_name(EXP_NAME)
+    runs = client.last_run_infos(exp.experiment_id)
+    last_run = max(runs, key=lambda x: x.end_time)
+    logged_artifacts = client.last_artifacts(last_run.run_id)
+    logged_model = logged_artifacts[0]
 
-    model_ds = model_dir.split('/')[-1]
+    mlflow.register_model(f"runs/{last_run.run_id}/{logged_model.path}", MODEL_NAME)
+
+    model = client.get_registered_model(MODEL_NAME)
+    latest_version = model.latest_versions[0]
+
+    local_path = client.get_model_version_download_uri(MODEL_NAME, latest_version.version)
+    model = mlflow.sklearn.load_model(local_path)
     score = model.score(val_X, val_y)
+    logger_metrics.info(f"got metric {score} for model {model_dir.split(' / ')[-1]}")
 
-    logger_metrics.info(f'got metric {score} for model {model_ds}')
+    exp = mlflow.get_experiment_by_name(EXP_NAME)
+    if not exp:
+        exp_id = mlflow.create_experiment(EXP_NAME)
+    else:
+        exp_id = exp.experiment_id
+
+    with mlflow.start_run(run_name="model_training", experiment_id=exp_id) as run:
+        mlflow.log_metric("mean_accuracy", score)
